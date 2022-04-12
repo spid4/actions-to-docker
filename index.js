615f7b23e8fdead8e990996c993d45ce779dcf27
spid4 <mikhilpasricha52@gmail.com>
2022-04-12 12:27:25 +0530

    lambda

---
+++ index.js
@@ -0,0 +1,6 @@
+
+// 'Hello World' nodejs6.10 runtime AWS Lambda function
+exports.handler = (event, context, callback) => {
+    console.log('Hello, logs!');
+    callback(null, 'great success');
+}
--- main.tf
+++ main.tf
@@ -14,4 +14,41 @@ provider "aws" {
 resource "aws_ecr_repository" "my_first_ecr_repo" {
   name = "my-first-ecr-repo" # Naming my repository
 }
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "archive_file" "lambda_zip" {
+    type          = "zip"
+    source_file   = "index.js"
+    output_path   = "lambda_function.zip"
+}
+
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+  runtime       = "nodejs14.x"
+
+  ephemeral_storage {
+    size = 10240 # Min 512 MB and the Max 10240 MB
+  }
+}
